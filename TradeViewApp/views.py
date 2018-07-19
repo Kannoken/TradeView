68bd48c5d18c45375e3c056e70d74742732dd95c
kannoken <imilya95@mail.ru>
2018-07-19 10:49:33 +0300

    data in html

--- TradeViewApp/views.py
+++ TradeViewApp/views.py
@@ -3,32 +3,33 @@ from TradeViewApp.models import Server, StoreInfo
 from TradeViewApp.forms import ServerForm
 import json
 import math
+import collections
 
 def main(request):
     form = ServerForm()
     server = Server.objects.all()
-    if request.method == 'GET':
-        pass
+    # if request.method == 'GET':
+    #     pass
     string_data = StoreInfo.objects.first()
-    create_data(string_data)
-    return render(request, 'index.html', {'form':form, 'server': server})
+    result = create_data(string_data)
+    return render(request, 'index.html', {'form':form, 'servers': server, 'result':reversed(sorted(result.items()))})
 
 def create_data(data):
-    result = []
-    tmp = []
+    result = {}
+    tmp = {}
     data = data.data.replace("\'", "\"")
     print(data)
     dic = json.loads(data)
     for key, value in dic.items():
         for k, val in value.items():
-            result.append(k)
-            tmp.append(k)
+            result[k] = {}
+            tmp[k] = {}
             for kk, v in val.items():
                 if 'close' in kk.lower():
                     result[k].update({'close':float(v)})
                 if 'open' in kk.lower():
                     tmp[k].update({'open': float(v)})
-            abs = math.fabs(float(tmp[k]['open']) - float(result[k]['close']))
-            percent = (tmp[k]['open']-result[k]['close'])/result[k]['close']*100
+            abs = round(float(tmp[k]['open']) - float(result[k]['close']), 2)
+            percent = round((tmp[k]['open']-result[k]['close'])/result[k]['close']*100, 2)
             result[k].update({'abs': abs, 'per': percent})
-    print(result)
+    return result
